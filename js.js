d87143a612cb6019bf8777b4f677b07adab098cc
NacerDev25 <nacerboutaldja96@gmail.com>
2026-02-02 21:05:23 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,16 @@
-window.onload = function() {
-    var communityHeading = document.getElementById('community-heading');
-    if (communityHeading) {
-        communityHeading.focus();
-    }
-};
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM loaded. Setting a timeout to focus.");
+    
+    setTimeout(function() {
+        var communityHeading = document.getElementById('community-heading');
+        
+        if (communityHeading) {
+            console.log("Timeout finished. Found community heading element:", communityHeading);
+            communityHeading.focus();
+            console.log("Focus method called. Active element is:", document.activeElement);
+        } else {
+            console.log("Timeout finished. Could not find community heading element.");
+        }
+    }, 100); // 100ms delay
+});
